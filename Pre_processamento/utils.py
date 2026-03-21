da6a49c9733290a6d9143e84c7d677b6105e569f
Copyplayerpuma <copyplayerpuma@gmail.com>
2026-03-21 16:04:58 -0300

    feat: add function names

---
+++ Pre_processamento/utils.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+#Function 1 that extracts the data from the excel file and returns it as a dataframe
+
+#Function 2 that defines the conditions
+
+#Function 3  that resolves the system of equations
+
+#Function 4 that finds a, b, c and d used to build the polinomials
+
+#Function 5 that generates the polinomials used to describe the lines
